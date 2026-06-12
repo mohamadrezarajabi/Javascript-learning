7bf90ae79ba99eeca8bf7fe80e437c1897fc61e1
Mohamad Reza <mohamadrezarajadi87@gmail.com>
2026-06-12 18:49:08 +0430

    lesson 003 data types

---
+++ lessons/003 - Data_types/js/app.js
@@ -0,0 +1,15 @@
+// String --> 'hello I'm mohamad reza' --> متن ها 
+// Number --> 10 , 12.5 , 41/5 --> عددها
+// Boolean --> 0 , 1 --> true , false
+//undefined --> تعریق نشده
+
+// To see the data type , we use typeof
+
+let age = 18;
+console.log(typeof age); // --> number
+
+let name = "mohamad";
+console.log(typeof name); // --> string
+
+let data;
+console.log(typeof data);
