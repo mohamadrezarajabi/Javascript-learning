0636cee364bb73f6db0caf099454a5f207d4d711
Mohamad Reza <mohamadrezarajadi87@gmail.com>
2026-06-13 18:28:35 +0430

    lesson 005 get data

---
+++ lessons/005 - get-data-from-user/js/app.js
@@ -0,0 +1,19 @@
+let age = +prompt("please enter the number");
+
+console.log(age);
+
+console.log(typeof age);
+
+alert(age); // alert: Display the information insade the console or project in ther user in a small window
+
+// null: If the user does not enter a string value , a null error in returned.
+
+let username = prompt("enter the username:", "amir"); 
+
+// NOTE:This value is set by default in the input ; Before the user enter any data.(amir)
+// It's like placeholder
+
+let isSeller = confirm("Are you a seller?"); // It only takes one input and output true or false.
+
+alert(isSeller); // the answer is Ok(true) or Cancel(false). 
+
