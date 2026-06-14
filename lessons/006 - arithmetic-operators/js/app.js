888555be7be79f10e8cc1b16a814a72cff573c81
Mohamad Reza <mohamadrezarajadi87@gmail.com>
2026-06-14 17:00:56 +0430

    lesson 006 Opertors

---
+++ lessons/006 - arithmetic-operators/js/app.js
@@ -0,0 +1,21 @@
+let num1 = 12;
+let num2 = 5;
+let num3 = 7;
+
+let plus = num1 + num2 + num3;
+let minus = num1 - num2 - num3;
+let multiplication = (num1 - num2) * num3; 
+let multiplication2 = num1 - num2 * num3; 
+let divsion = num1 / num2 / num3;
+let power = num1 ** num2;
+let remaining = num1 % num2;
+
+console.log(plus);
+console.log(minus);
+console.log(multiplication);
+console.log(multiplication2);
+console.log(divsion);
+console.log(power);
+console.log(remaining);
+
+// bracket > power > multiplication & divsion > plus & minus
