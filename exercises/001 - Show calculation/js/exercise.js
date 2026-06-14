18b2cdf74cc582e52aa132b548026ac2f6c0e395
Mohamad Reza <mohamadrezarajadi87@gmail.com>
2026-06-14 17:20:20 +0430

    exercise 001

---
+++ exercises/001 - Show calculation/js/exercise.js
@@ -0,0 +1,10 @@
+let num1 = +prompt("please enter the number:", 0);
+let num2 = +prompt("please enter the number:", 0);
+
+let plus = num1 + num2;
+let minus = num1 - num2;
+let multi = num1 * num2;
+let divsion = num1 / num2;
+
+alert(`plus: ${plus}, minus: ${minus}, multi: ${multi}, divsion: ${divsion}`);
+
