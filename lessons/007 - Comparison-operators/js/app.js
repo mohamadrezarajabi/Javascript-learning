51f676b01977e4ae1a70f611c39628f3658b23ba
Mohamad Reza <mohamadrezarajadi87@gmail.com>
2026-06-15 16:28:09 +0430

    lesson 007 coparison operators

---
+++ lessons/007 - Comparison-operators/js/app.js
@@ -0,0 +1,35 @@
+let num1 = 10;
+let num2 = 15;
+
+console.log(num1 == num2); //false
+
+// NOTE: COmparison operators only check values, not data type
+
+console.log(num1 == "10"); //true
+
+// NOTE:if we want to check both the Value and data type we use ===
+
+console.log(num1 === "10"); // false
+
+//---------------------------------------------------------------
+
+console.log(num1 != num2); //true
+
+
+console.log(num1 != "10"); //false
+
+
+console.log(num1 !== "10"); // true
+
+//---------------------------------------------------------------
+
+console.log(num1 < num2); //true
+
+
+console.log(num1 > num2); //false
+
+
+console.log(num1 <= num2); // true
+
+console.log(num1 >= num2); // false
+
