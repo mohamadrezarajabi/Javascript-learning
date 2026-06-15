834e7ebbe36c5576f37fb63573de5f167eafcbd5
Mohamad Reza <mohamadrezarajadi87@gmail.com>
2026-06-15 17:04:05 +0430

    exercise 004 average

---
+++ exercises/004 - average/js/exercise.js
@@ -0,0 +1,7 @@
+let number = +prompt("please the Enter the frist number:" , 1);
+let number2 = +prompt("please the Enter the second number:" , 1);
+let number3 = +prompt("please the Enter thethrid number:" , 1);
+
+average = (number + number2 + number3) / 3
+
+alert("average" + " " + average)
