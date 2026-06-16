3fa8a43ab3edd3533534a845d29b4bd08e5abe48
Mohamad Reza <mohamadrezarajadi87@gmail.com>
2026-06-16 16:46:29 +0430

    lesson 009 conditional2

---
+++ lessons/009 - conditional2/js/app.js
@@ -0,0 +1,10 @@
+let num1 = 10;
+let num2 = 15;
+
+if (num1 > num2){ //if
+    alert("num1 بزرگتر از num2هست"); 
+} else if (num2 > num1){ // or if
+    alert("num2 بزرگتر از num1هست");
+} else { // Otherwise
+    alert("num1 مساوی هست با num2");
+}
