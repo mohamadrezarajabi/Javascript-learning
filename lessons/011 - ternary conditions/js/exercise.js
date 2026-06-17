842e3d86ccd1ea7eef37cffbf209493f972215ac
Mohamad Reza <mohamadrezarajadi87@gmail.com>
2026-06-17 14:39:12 +0430

    lesson 011 termary condition

---
+++ lessons/011 - ternary conditions/js/exercise.js
@@ -0,0 +1,23 @@
+// if we want to use small conditions that only have an if and else we use (ternary-conditions).
+
+let num1 = 10;
+let num2 = 5;
+
+// if (num1 * num2 === 10){
+//     alert("THe sum is equal to 10.")
+// } else {
+//     alert("THe sum is equal to 50.")
+// }
+
+
+//  ^
+//  |  
+// Normal Condition
+
+// Ternary condiciton
+//  |
+//  U
+
+// cond ? ifLogic : elseLogic --> Cond ternary
+
+num1 * num2 === 10 ? alert("THe sum is equal to 10.") : alert("THe sum is equal to 50.")
