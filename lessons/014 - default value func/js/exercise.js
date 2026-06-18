639b3d17395afd54116e77973601e08363894054
Mohamad Reza <mohamadrezarajadi87@gmail.com>
2026-06-18 22:19:22 +0430

    lesson 014 defualt value func

---
+++ lessons/014 - default value func/js/exercise.js
@@ -0,0 +1,10 @@
+const number = +prompt("enter frist number: ");
+const number2 = +prompt("enter second number: ");
+
+function sum(num1 = 0, num2 = 0){ // Default Value
+    f = num1 + num2
+    return f
+}
+
+alert(sum());  // <-- if we leave the () empty, the Zero and Zero will br added --> 0
+
