0d2639b795d9764157e7d1ff773fb0f32203b7e6
Mohamad Reza <mohamadrezarajadi87@gmail.com>
2026-06-21 12:36:30 +0430

    Update exc 020 reCAPTCHA

--- exercises/020 - reCAPTCHA/js/exercise.js
+++ exercises/020 - reCAPTCHA/js/exercise.js
@@ -1,7 +1,7 @@
 const random = (Math.floor((Math.random() * 100000) + 10000))
 
 
-let UserCap = prompt("Please enter the Captcha: " + random)
+let UserCap = +prompt("Please enter the Captcha: " + random);
 
 if (UserCap === random){
     alert("Successful");
