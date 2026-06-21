ff389e26153141670310165b6c0b4807cac96ccf
Mohamad Reza <mohamadrezarajadi87@gmail.com>
2026-06-21 14:57:50 +0430

    lesson 020 Assignment Operators

---
+++ lessons/020 - Assignment Operators/js/app.js
@@ -0,0 +1,15 @@
+// Assignment Operators --> shortcut for updating a variable's value
+
+let number = 10;
+
+number += 5;   // number = number + 5  --> 15
+number -= 2;   // number = number - 2  --> 8
+number /= 2;   // number = number / 2  --> 5
+number *= 2;   // number = number * 2  --> 20
+number **= 2;  // number = number ** 2 --> 100
+
+// ++ and -- | --> add or subtract exactly 1
+number++;  // number = number + 1
+++number;  // same result, just different order of execution
+number--;  // number = number - 1
+--number;  // same result, just different order of execution
