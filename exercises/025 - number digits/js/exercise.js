e1d478d3336932e5ef51a37ddaff33d3bc09fff2
Mohamad Reza <mohamadrezarajadi87@gmail.com>
2026-06-22 20:49:23 +0430

    Update exc  025 number digits

--- exercises/025 - number digits/js/exercise.js
+++ exercises/025 - number digits/js/exercise.js
@@ -1,10 +1,29 @@
-let number = Math.floor(+prompt("Enter the number: "));
+// let number = Math.floor(+prompt("Enter the number: "));
 
-if (!isNaN(number)){
-    number = String(number).trim();
-    len = number.length;
-    numberLenght = Number(len);
-    alert(numberLenght);
+// if (!isNaN(number)){
+//     number = String(number).trim();
+//     len = number.length;
+//     numberLenght = Number(len);
+//     alert(numberLenght);
+// } else {
+//     alert("please enter the number ");
+// }
+
+// ----------- or ------------
+
+let number = +prompt("Enter the number: ");
+let len = 0;
+
+if (!isNaN(number)) {
+    if (number === 0) {
+        len = 1;
+    } else{
+        for(let i = 0; number / 10 !== 0;i++) {
+            len++;
+            number = Math.floor(number /= 10);
+        }
+        alert(len);
+    }
 } else {
     alert("please enter the number ");
 }
