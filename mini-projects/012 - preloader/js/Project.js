26a3be2470db45f3ea43b991ebf1478714548b7d
Mohamad Reza <mohamadrezarajadi87@gmail.com>
2026-09-10 16:52:39 +0330

    MP 012 Preloader

---
+++ mini-projects/012 - preloader/js/Project.js
@@ -0,0 +1,6 @@
+const loader = document.querySelector(".loader");
+
+function Showloading() {
+  document.querySelector(".loader").classList.add("hidden");
+  document.body.classList.remove("loading");
+}
